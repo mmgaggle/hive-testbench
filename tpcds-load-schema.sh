ccf224d7d065595731c52166e722072caed63e15
Kyle Bader <kbader@redhat.com>
2019-01-17 12:35:14 -0800

    bad path

--- tpcds-load-schema.sh
+++ tpcds-load-schema.sh
@@ -12,7 +12,7 @@ function usage {
 function load_tables {
   for table in ddl-tpcds/bin_partition_create_only/* ;do
     hive -i settings/load-flat.sql \
-         -f ddl-tpcds/bin_partitioned/${i} \
+         -f ${i} \
          -d DB=tpcds_bin_partitioned_parquet_1000 \
          -d LOCATION=s3a://tpc/parquet/1000 \
          -d FILE=parquet
